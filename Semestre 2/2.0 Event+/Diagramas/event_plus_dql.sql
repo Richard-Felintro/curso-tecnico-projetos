70e2215c0e3bcf98e2122ee3e0f200997e787ec2
Richard Felintro <richardfelintro06@gmail.com>
2023-08-11 16:31:17 -0300

    Banco de dados do Event+ finalizado.

--- Semestre 2/2.0 Event+/Diagramas/event_plus_dql.sql
+++ Semestre 2/2.0 Event+/Diagramas/event_plus_dql.sql
@@ -1,17 +1,37 @@
 -- DQL - Data Query Language
 USE EventPlus
 
--- Mostrar todos os comentários referentes ao Evento de C# em 2023/07/25 e seus comentantes.
-SELECT 
-Usuario.Nome,
-Comentario.Conteudo AS Comentário
+-- Criar script para consulta exibindo os seguintes dados.
+/*Usar JOIN
 
-FROM
-TipoDeUsuario,
-Usuario,
-Comentario
+Nome do usuário
+Tipo do usuário
+Data do evento
+Local do evento (Instituição)
+Titulo do evento
+Nome do evento
+Descrição do evento
+Situação do evento
+Comentário do evento
+*/
+
+SELECT 
+Usuario.Nome AS Username,
+TipoDeUsuario.TituloTipoUsuario AS Usertype,
+Evento.Nome AS 'Evento',
+Evento.Descricao AS 'Descrição',
+Evento.DataEvento AS 'Data do Evento',
+Instituicao.Endereco AS 'Endereço',
+CASE PresencaEmEvento.Confirmacao WHEN 0 THEN 'Ausente' WHEN 1 THEN 'Presente' END AS 'Presença',
+Comentario.Conteudo AS 'Comentário'
+
+FROM 
+TipoDeUsuario
+LEFT JOIN Usuario ON TipoDeUsuario.IdTipoUsuario = Usuario.IdTipoUsuario
+LEFT JOIN PresencaEmEvento ON PresencaEmEvento.IdUsuario = Usuario.IdUsuario
+LEFT JOIN Evento ON Evento.IdEvento = PresencaEmEvento.IdEvento
+LEFT JOIN Comentario ON (Comentario.IdUsuario = Usuario.IdUsuario AND Comentario.IdEvento = Evento.IdEvento)
+LEFT JOIN Instituicao ON Instituicao.IdInstituicao = Evento.IdInsituicao
 
 WHERE
-TipoDeUsuario.IdTipoUsuario = Usuario.IdTipoUsuario AND
-Comentario.IdUsuario = Usuario.IdUsuario AND
-Comentario.IdEvento = 1
+Usuario.IdUsuario = 2
