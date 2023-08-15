11edce1c8eee2090ea9ea6f06b6f31dbfd8be5f5
Richard Felintro <richardfelintro06@gmail.com>
2023-08-15 15:19:04 -0300

    Desafios extras concluídos.

--- Semestre 2/2.1 Projeto Health Clinic/senai_HealthClinic_01_DML.sql
+++ Semestre 2/2.1 Projeto Health Clinic/senai_HealthClinic_01_DML.sql
@@ -4,18 +4,18 @@ USE HealthClinic
 INSERT INTO Clinica VALUES 
 	(12345678901234,'Rua dos Bobos, 0','Rede Hospitalar de Consultas Health Clinic','Health Clinic','06:00','22:00')
 INSERT INTO Paciente VALUES 
-	('João Pedro','joaopedro06@gmail.com','joao1234'),
-	('Astunfo','astunfo1234@hotmail.com','12345678'),
-	('Arnaldina','arna33@gmail.com','1a2b3c4d'),
-	('Vitória','derrota@gmail.com','12345abcde'),
-	('Menelique II','reunifiqueiaetiopia@gmail.com','12341234')
+	('João Pedro','joaopedro06@gmail.com','joao1234','2005-04-05'),
+	('Astunfo','astunfo1234@hotmail.com','12345678','1996-08-13'),
+	('Arnaldina','arna33@gmail.com','1a2b3c4d','1989-05-09'),
+	('Vitória','derrota@gmail.com','12345abcde','1999-09-09'),
+	('Menelique II','reunifiqueiaetiopia@gmail.com','12341234','2003-05-23')
 INSERT INTO Medico VALUES
-	(1,'Jotuncio Neves','jneves666@gmail.com','JN12345678'),
-	(1,'Cláudio João','automotivaclaudio3k@gmail.com','Claudio6969'),
-	(1,'Jetumbia Augusta','jetaug33@gmail.com','1234567890')
+	(1,'Jotuncio Neves','jneves666@gmail.com','JN12345678','1998-12-21'),
+	(1,'Cláudio João','automotivaclaudio3k@gmail.com','Claudio6969','1988-10-25'),
+	(1,'Jetumbia Augusta','jetaug33@gmail.com','1234567890','2000-09-08')
 INSERT INTO Administrador VALUES
-	(1,'Washington Rothschild','ricoprakrl@gmail.com','KRL123123'),
-	(1,'Melon Husk','minasdecobalto@gmail.com','musk123456')
+	(1,'Washington Rothschild','ricoprakrl@gmail.com','KRL123123','1988-06-15'),
+	(1,'Melon Husk','minasdecobalto@gmail.com','musk123456','1973-04-29')
 INSERT INTO Consulta VALUES
 	(1,1,3,'02-08-2023','08:00','Exame de urina realizado, tudo de acordo.'),
 	(1,2,2,'23-07-2023','15:00','Check-Up básico, tudo de acordo.'),
--- Semestre 2/2.1 Projeto Health Clinic/senai_HealthClinic_01_DQL.sql
+++ Semestre 2/2.1 Projeto Health Clinic/senai_HealthClinic_01_DQL.sql
@@ -1,2 +1,20 @@
 -- DQL - Data Query Language
+USE HealthClinic
 
+--Criar função para retornar a quantidade de médicos de uma determinada especialidade
+SELECT
+M.Nome AS Nome,
+M.Email AS Email
+
+FROM Medico AS M
+LEFT JOIN JoinEspecialidade AS J ON J.IdMedico = M.IdMedico
+WHERE J.IdEspecialidade = 2
+
+--Criar procedure para retornar a idade de um determinado usuário específico
+USE HealthClinic
+
+SELECT 
+Paciente.Nome,
+DATEDIFF(YEAR, Paciente.DataNascimento, GETDATE()) AS Idade
+FROM Paciente
+WHERE Paciente.IdPaciente = 3
